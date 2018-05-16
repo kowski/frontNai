00059c0c74a64caea1453252fabd4debf2ca9c13
Krystian Kalkowski <krystian.kalkowski@hycom.pl>
2018-05-16 11:33:58 +0200

    Added bootstrap dependency

--- src/Components/SupplierComponent.js
+++ src/Components/SupplierComponent.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'; 
-import axios from 'axios';
-
-class SupplierComponent extends Component{
-	state={
-		suppliers: []
+	import React, { Component } from 'react';
+	import axios from 'axios';
+	import {Button} from 'react-bootstrap';
+	import {Table} from 'react-bootstrap';
+	class SupplierComponent extends Component{
+		state={
+			suppliers: []
+		}
+		suppliers:[]
+		componentDidMount(){axios.get('https://jsonplaceholder.typicode.com/users')
+		.then(res => {
+			const suppliers= res.data;
+			this.setState({suppliers});
+			this.setSuppliers({suppliers});
+			console.log("suppliers:" + this.state.suppliers);
+		})
 	}
-
-	componentDidMount(){     axios.get('https://jsonplaceholder.typicode.com/users')
-	.then(res => {
-		const suppliers= res.data;
-		this.setState({suppliers});
-		console.log("suppliers:" + this.state);
-	})
-}
-render(){ 
-	return(
-		 
-		<ul>
-        { this.state.suppliers.map(person => <li>{person.name}</li>)}
-     	</ul>
-
-
-		)
+	 getSuppliers(){
+		return this.suppliers;
 	}
-}
-export default SupplierComponent;
+	render(){
+		console.log(this.suppliers);
+		return(
+			<Table responsive>
+				<thead>
+				<tr>
+				<th>#</th>
+				<th>Table heading</th>
+				<th>Table heading</th>
+				<th>Table heading</th>
+				<th>Table heading</th>
+				<th>Table heading</th>
+				<th>Table heading</th>
+				</tr>
+				</thead>
+			{ this.state.suppliers.map(person => (<tbody><tr><td>{person.name}</td></tr></tbody>))}
+				</Table>
+				)
+		}
+	}
+	export default SupplierComponent;
